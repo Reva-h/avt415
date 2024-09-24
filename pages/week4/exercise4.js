454bce11b43d7c6de0636dbf4135a4ecd14419a8
RevaH <RevaH@MacBook-Air-8.local>
2024-09-23 20:43:23 -0400

    fixed confetti onclick effect

--- pages/week4/exercise4.js
+++ pages/week4/exercise4.js
@@ -20,167 +20,78 @@ function updateMessage() {
   // Run the function whenever the window is resized
   window.onresize = updateMessage;
 
-/* below confetti code from https://codepen.io/lixelart/pen/KKwXQeW --------------------------------------------------------*/
-  function addClass(el, className) {
-    el.classList.add(className);
-  }
-  function removeClass(el, className) {
-    el.classList.remove(className);
-  }
+// Setup the canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let dpr = window.devicePixelRatio || 1;  // Device Pixel Ratio for sharper rendering
+canvas.width = window.innerWidth * dpr;
+canvas.height = window.innerHeight * dpr;
+ctx.scale(dpr, dpr);
 
-  const DECAY = 4;
-  const SPREAD = 50;
-  const GRAVITY = 1200;
+// Confetti properties
+const confettiParticles = [];
+const confettiColors = ['#ffffff'];  // White color for all confetti
+const gravity = 0.5; // Falling speed of confetti
 
-  let angle = 270; // Changes angle of confetti shot.
-  let shoot = false;
-
-  let dpr = window.devicePixelRatio || 2; // 2 is nice & large confetti. Original was 1
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-
-  let confettiSpriteIds = [];
-  let confettiSprites = {};
-
+// Handle window resizing
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth * dpr;
+  canvas.height = window.innerHeight * dpr;
   ctx.scale(dpr, dpr);
+});
 
-  function setCanvasSize() {
-    canvas.width = window.innerWidth * dpr;
-    canvas.height = window.innerHeight * dpr;
-    // canvas.style.width = window.innerWidth + 'px';
-    // canvas.style.height = window.innerHeight + 'px';
-  }
+// Function to create a confetti particle
+function createConfetti(x, y) {
+  for (let i = 0; i < 100; i++) {
+    // Random offsets for more natural spread around the click point
+    const offsetX = Math.random() * 100 - 50; // Spread horizontally
+    const offsetY = Math.random() * 100 - 50; // Spread vertically
 
-  function setupListeners() {
-    // Use TweenLite tick event for the render loop
-    TweenLite.ticker.addEventListener('tick', render);
-    
-    // switch to window.onload
-    canvas.addEventListener('mousedown', handleMousedown);
-    canvas.addEventListener('mouseup', handleMouseup);
-    canvas.addEventListener('resize', setCanvasSize);
-  }
-
-  function handleMousedown() {
-    shoot = true;
-  }
-  function handleMouseup() {
-    shoot = false;
-  }
-
-  function addConfettiParticles(amount, angle, velocity, x, y) {
-    let i = 0;
-    while (i < amount) {
-        // sprite
-        const r = _.random(4, 10) * dpr;
-        const d = _.random(15, 25) * dpr;
-        
-        const cr = _.random(100, 255);
-        const cg = _.random(100, 255);
-        const cb = _.random(100, 255);
-        const color = `rgb(${cr}, ${cg}, ${cb})`;
-        
-        const tilt = _.random(10, -10);
-        const tiltAngleIncremental = _.random(0.07, 0.05);
-        const tiltAngle = 0;
-
-        const id = _.uniqueId();
-        const sprite = {
-            [id]: {
-                angle,
-                velocity,
-                x,
-                y,
-                r,
-                d,
-                color,
-                tilt,
-                tiltAngleIncremental,
-                tiltAngle,
-            },
-        };
-
-        Object.assign(confettiSprites, sprite);
-        confettiSpriteIds.push(id);
-        tweenConfettiParticle(id);
-        i++;
-    }
-  }
-
-  function tweenConfettiParticle(id) {
-    const minAngle = confettiSprites[id].angle - SPREAD / 2;
-    const maxAngle = confettiSprites[id].angle + SPREAD / 2;
-    
-    const minVelocity = confettiSprites[id].velocity / 4;
-    const maxVelocity = confettiSprites[id].velocity;
-
-    // Physics Props
-    const velocity = _.random(minVelocity, maxVelocity);
-    const angle = _.random(minAngle, maxAngle);
-    const gravity = GRAVITY;
-    const friction = _.random(0.01, 0.05);
-    const d = 0;
-
-    TweenLite.to(confettiSprites[id], DECAY, {
-        physics2D: {
-            velocity,
-            angle,
-            gravity,
-            friction,
-        },
-        d,
-        ease: Power4.easeIn,
-        onComplete: () => {
-            // remove confetti sprite and id
-            _.pull(confettiSpriteIds, id);
-            delete confettiSprites[id];
-        },
+    confettiParticles.push({
+      x: x + offsetX, // Apply random horizontal offset
+      y: y + offsetY, // Apply random vertical offset
+      size: Math.random() * 10 + 5, // Random size between 5 and 15
+      color: confettiColors[Math.floor(Math.random() * confettiColors.length)],
+      velocityX: Math.random() * 4 - 2, // More random horizontal speed
+      velocityY: Math.random() * -10 - 3, // More random vertical speed
+      rotation: Math.random() * 360, // Rotation angle
+      rotationSpeed: Math.random() * 10 - 5, // How fast the confetti rotates
     });
   }
+}
 
-  function updateConfettiParticle(id) {
-    const sprite = confettiSprites[id];
-    
-    const tiltAngle = 0.0005 * sprite.d;
-    
-    sprite.angle += 0.01;
-    sprite.tiltAngle += tiltAngle;
-    sprite.tiltAngle += sprite.tiltAngleIncremental;
-    sprite.tilt = (Math.sin(sprite.tiltAngle - (sprite.r / 2))) * sprite.r * 2;
-    sprite.y += Math.sin(sprite.angle + sprite.r / 2) * 2;
-    sprite.x += Math.cos(sprite.angle) / 2;
-  }
+// Function to update and draw confetti
+function updateConfetti() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  confettiParticles.forEach((particle, index) => {
+    // Apply gravity and update positions
+    particle.velocityY += gravity;
+    particle.x += particle.velocityX;
+    particle.y += particle.velocityY;
+    particle.rotation += particle.rotationSpeed;
 
-  function drawConfetti() {
-    confettiSpriteIds.map(id => {
-        const sprite = confettiSprites[id];
-        
-        ctx.beginPath();
-        ctx.lineWidth = sprite.d / 2;
-        ctx.strokeStyle = sprite.color;
-        ctx.moveTo(sprite.x + sprite.tilt + sprite.r, sprite.y);
-        ctx.lineTo(sprite.x + sprite.tilt, sprite.y + sprite.tilt + sprite.r);
-        ctx.stroke();
-
-        updateConfettiParticle(id);
-    });
-  }
-
-  function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    drawConfetti();
-  }
-
-  function shootConfetti() {
-    requestAnimationFrame(shootConfetti);
-    if (shoot) {
-        addConfettiParticles(20, angle, 3500, canvas.width/2, canvas.height+75);   
+    // Remove confetti that falls off the screen
+    if (particle.y > canvas.height || particle.x < 0 || particle.x > canvas.width) {
+      confettiParticles.splice(index, 1);
+    } else {
+      // Draw confetti
+      ctx.save();
+      ctx.translate(particle.x, particle.y);
+      ctx.rotate((particle.rotation * Math.PI) / 180);
+      ctx.fillStyle = particle.color;
+      ctx.fillRect(-particle.size / 2, -particle.size / 2, particle.size, particle.size);
+      ctx.restore();
     }
-  }
+  });
 
-  setupListeners();
-  setCanvasSize();
+  // Continue updating
+  requestAnimationFrame(updateConfetti);
+}
 
-  shootConfetti();
-  
+// Start animation loop
+updateConfetti();
+
+// Add click event to trigger confetti burst
+window.addEventListener('click', (event) => {
+  createConfetti(event.clientX, event.clientY); // Create confetti at click position
+});
